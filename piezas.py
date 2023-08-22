bb76e8300efb9902c46cf9ab3f0840430ecd0624
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-08-22 03:03:20 -0300

    file to store tiles

---
+++ piezas.py
@@ -0,0 +1,33 @@
+
+DATA= [
+    {"letter": "A", "value": 1, "quantity": 12},
+    {"letter": "B", "value": 3, "quantity": 2},
+    {"letter": "C", "value": 3, "quantity": 4},
+    {"letter": "D", "value": 2, "quantity": 5},
+    {"letter": "E", "value": 1, "quantity": 12},
+    {"letter": "F", "value": 4, "quantity": 1},
+    {"letter": "G", "value": 2, "quantity": 2},
+    {"letter": "H", "value": 4, "quantity": 2},
+    {"letter": "I", "value": 1, "quantity": 6},
+    {"letter": "J", "value": 8, "quantity": 1},
+    {"letter": "L", "value": 1, "quantity": 4},
+    {"letter": "M", "value": 3, "quantity": 2},
+    {"letter": "N", "value": 1, "quantity": 5},
+    {"letter": "Ñ", "value": 8, "quantity": 1},
+    {"letter": "O", "value": 1, "quantity": 9},
+    {"letter": "P", "value": 3, "quantity": 2},
+    {"letter": "Q", "value": 5, "quantity": 1},
+    {"letter": "R", "value": 1, "quantity": 5},
+    {"letter": "S", "value": 1, "quantity": 6},
+    {"letter": "T", "value": 1, "quantity": 4},
+    {"letter": "U", "value": 1, "quantity": 5},
+    {"letter": "V", "value": 4, "quantity": 1},
+    {"letter": "X", "value": 8, "quantity": 1},
+    {"letter": "Y", "value": 4, "quantity": 1},
+    {"letter": "Z", "value": 10, "quantity": 1},
+    {"letter": "CH", "value": 5, "quantity": 1},
+    {"letter": "LL", "value": 8, "quantity": 1},
+    {"letter": "RR", "value": 8, "quantity": 1},
+    {"letter": "_", "value": 0, "quantity": 2}
+  ]
+  
