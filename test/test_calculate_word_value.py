57755a8c982ac45470edf006e391602255065e4b
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-09-06 01:00:57 -0300

    changed importation of the calculate word value method

--- test/test_calculate_word_value.py
+++ test/test_calculate_word_value.py
@@ -1,21 +1,24 @@
 import unittest
-from game.board import calculate_word_value
+from game.board import Board
 from game.board import Cell
 from game.tiles import Tile
 
 
 class TestCalculateWordValue(unittest.TestCase):
     def test_simple(self):
+        board = Board()
         word = [
             Cell(letter=Tile('C', 1)),
             Cell(letter=Tile('A', 1)),
             Cell(letter=Tile('S', 2)),
             Cell(letter=Tile('A', 1)),
         ]
-        value = calculate_word_value(word)
+        
+        value = board.calculate_word_value(word)
         self.assertEqual(value, 5)
 
     def test_with_letter_multiplier(self):
+        board = Board()
         word = [
             Cell(letter=Tile('C', 1)),
             Cell(letter=Tile('A', 1)),
@@ -26,10 +29,11 @@ class TestCalculateWordValue(unittest.TestCase):
             ),
             Cell(letter=Tile('A', 1)),
         ]
-        value = calculate_word_value(word)
+        value = board.calculate_word_value(word)
         self.assertEqual(value, 7)
 
     def test_with_word_multiplier(self):
+        board = Board()
         word = [
             Cell(letter=Tile('C', 1)),
             Cell(letter=Tile('A', 1)),
@@ -40,10 +44,11 @@ class TestCalculateWordValue(unittest.TestCase):
             ),
             Cell(letter=Tile('A', 1)),
         ]
-        value = calculate_word_value(word)
+        value = board.calculate_word_value(word)
         self.assertEqual(value, 10)
 
     def test_with_letter_word_multiplier(self):
+        board = Board()
         word = [
             Cell(
                 multiplier=3,
@@ -58,11 +63,11 @@ class TestCalculateWordValue(unittest.TestCase):
             ),
             Cell(letter=Tile('A', 1)),
         ]
-        value = calculate_word_value(word)
+        value = board.calculate_word_value(word)
         self.assertEqual(value, 14)
 
     def test_with_letter_word_multiplier_no_active(self):
-        # QUE HACEMOS CON EL ACTIVE ????
+        board = Board()
         word = [
             Cell(
                 multiplier=3,
@@ -77,8 +82,9 @@ class TestCalculateWordValue(unittest.TestCase):
             ),
             Cell(letter=Tile('A', 1)),
         ]
-        value = calculate_word_value(word)
-        self.assertEqual(value, 5)
-    
+        value1 = board.calculate_word_value(word)    # Calcula el valor con multiplicadores
+        value2 = board.calculate_word_value(word)    # Calcula el valor sin multiplicadores
+        self.assertEqual(value1, 14)
+        self.assertEqual(value2, 7)
 if __name__ == '__main__':
     unittest.main()
