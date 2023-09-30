db4e07d9cfddf5ccf144692ce3209697e5dd9198
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-09-30 16:14:39 -0300

    added letter and word multiplier

--- game/board.py
+++ game/board.py
@@ -22,7 +22,7 @@ class Board:
             [ Cell(1, '') for _ in range(15) ]
             for _ in range(15)
         ]
-    
+
     def calculate_word_value(self, word):
         word_value = 0
         word_multiplier = 1
@@ -34,7 +34,6 @@ class Board:
             if cell.multiplier_type == 'word':
                 word_multiplier *= cell.multiplier
                 cell.multiplier = 1  # Reinicia el multiplicador de la celda a 1
-
         word_value *= word_multiplier
         return word_value
     
@@ -45,3 +44,50 @@ class Board:
             return False
         else:
             return True
+
+    def positions(self):
+        self.letter_multiplier()
+        self.word_multiplier()
+
+    def word_multiplier(self):
+        word_multi = [
+            (0,0), (7,0), (14,0), (0,7), (0,14), (7,14), (14,14)
+        ]
+        for i in range(15):
+            for j in range(15):
+                cell = self.grid[i][j]
+                if (i,j) in word_multi:
+                    cell.multiplier = 3
+                    cell.multiplier_type = 'word'
+                    
+        for i in range(15):
+            for j in range(15):
+                not_there = [0, 5, 6, 7, 8, 9, 14]
+                cell = self.grid[i][j]
+                if i == j or (i + j == 14):
+                    if (i and j) not in not_there:
+                        cell.multiplier = 2
+                        cell.multiplier_type = 'word'
+
+    def letter_multiplier(self):
+        letter_multi_2 = [
+            (3,0), (11,0), (6,2), (8,2), (0,3), (14,3), (7,3),
+            (2,6), (6,6), (8,6), (12,6), (3,7), (11,7), (2,8),
+            (6,8), (8,8), (12,8), (14,11), (0,11), (7,11), (6,12),
+            (8,12), (11,14), (3,14), 
+        ]
+
+        letter_multi_3 = [
+            (1,5), (1,9), (5,1), (5,5), (5,9), (5,13), (9,1),
+            (9,5), (9,9), (9,13), (13,5), (13,9),
+        ]
+        for i in range(15):
+            for j in range(15):
+                cell = self.grid[i][j]
+                if (i,j) in letter_multi_2:
+                    cell.multiplier = 2
+                    cell.multiplier_type = 'letter'
+                if (i,j) in letter_multi_3:
+                    cell.multiplier = 3
+                    cell.multiplier_type = "letter"
+
