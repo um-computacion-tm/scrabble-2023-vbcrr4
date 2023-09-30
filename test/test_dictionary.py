1f5fd7fb527712bb0f446487b2c08cb690fb9d05
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-09-29 23:04:11 -0300

    changed test

--- test/test_dictionary.py
+++ test/test_dictionary.py
@@ -1,19 +1,27 @@
 import unittest
+from unittest.mock import patch, Mock
 from game.dictionary import Dictionary
 
 class TestDictionary(unittest.TestCase):
-    def setUp(self):
-        self.dictionary = Dictionary()
+    @patch('pyrae.dle.search_by_word')
+    def test_get_word_dictionary(self, mock_search_by_word):
+        # Configura el comportamiento simulado de la función mock
+        mock_search_by_word.return_value = Mock(to_dict=lambda: {'title': ''})
 
-    def test_word_exists(self):
-        word = "casa"
-        result = self.dictionary.validate_word(word)
-        self.assertTrue(result)
+        scrabble_game = Dictionary()
+        word = "Alambrado"
+        valor = scrabble_game.get_word(word)
+        self.assertTrue(valor)
 
-    def test_word_does_not_exist(self):
-        word = "xyzabc"
-        result = self.dictionary.validate_word(word)
-        self.assertFalse(result)
+    @patch('pyrae.dle.search_by_word')
+    def test_get_word_dictionary_not(self, mock_search_by_word):
+        # Configura el comportamiento simulado de la función mock
+        mock_search_by_word.return_value = Mock(to_dict=lambda: {'title': 'Diccionario de la lengua española | Edición del Tricentenario | RAE - ASALE'})
+
+        scrabble_game = Dictionary()
+        word = "Sanacaramalana"
+        valor = scrabble_game.get_word(word)
+        self.assertFalse(valor)
 
 if __name__ == '__main__':
     unittest.main()
