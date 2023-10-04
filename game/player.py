0630de568d15334eb62c2e6b6826282e4c03c4f1
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-10-04 10:04:55 -0300

    added play_tile and display_hand

--- game/player.py
+++ game/player.py
@@ -1,4 +1,4 @@
-from game.tiles import BagTiles
+from game.tiles import Tile
 class Player:
     def __init__(self,bag_tiles = None):
         self.bag_tiles = bag_tiles
@@ -10,4 +10,12 @@ class Player:
     
     def reset(self):
         self.tiles = []  # Restablece las fichas del jugador.
-
+        
+    def play_tile(self, tile):
+        if tile in self.tiles:
+            self.tiles.remove(tile)
+        else:
+            print("Tile not found in player's hand.")
+    
+    def display_hand(self):
+        print("Player's hand: ", self.tiles.__str__())
