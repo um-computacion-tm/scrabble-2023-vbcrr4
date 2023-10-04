a8852254d97dc5926d035b6d7819f4ce2efb5862
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-10-04 10:03:03 -0300

    added log_level

--- game/dictionary.py
+++ game/dictionary.py
@@ -1,6 +1,7 @@
 from pyrae import dle
 
 class Dictionary:
+    dle.set_log_level(log_level='CRITICAL')
     def get_word(self, word):
         res = dle.search_by_word(word)
         result = res.to_dict()
