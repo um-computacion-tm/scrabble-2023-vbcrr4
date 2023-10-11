6b4297fa3e9f7b854a3fb9b118e3f71048855059
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-10-11 12:43:10 -0300

    added main and scrabblecli folders

---
+++ game/main.py
@@ -0,0 +1,8 @@
+from game.scrabbleCli import ScrabbleCli
+
+def main():
+    client = ScrabbleCli()
+    client.client()
+
+if __name__ == '__main__':
+    main()
---
+++ game/scrabbleCli.py
@@ -0,0 +1,16 @@
+class ScrabbleCli:
+    def get_player_count():
+        while True:
+            try:
+                player_count = int(input('Cantidad de jugadores (1-3): '))
+                if 1 <= player_count <= 3:
+                    break
+                else:
+                    print('Número de jugadores no válido. Intente de nuevo.')
+            except ValueError:
+                print('Ingrese un número válido por favor.')
+
+        return player_count
+
+
+    
