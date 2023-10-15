a99fa519856be86a100bcd2089d5f125c806b416
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-10-14 21:35:57 -0300

    changed code

--- game/scrabbleCli.py
+++ game/scrabbleCli.py
@@ -1,16 +1,26 @@
 from game.scrabble_game import ScrabbleGame
+
 class ScrabbleCli:
-        def __init__(self): 
-            self.game = ScrabbleGame(players_count=0)
-        def get_player_count(self):
-            while True:
-                try:
-                    player_count = int(input('Cantidad de jugadores (1-3): '))
-                    if 1 <= player_count <= 3:
-                        break
-                    else:
-                        print('Número de jugadores no válido. Intente de nuevo.')
-                except ValueError:
-                    print('Ingrese un número válido por favor.')
-            return player_count
-        
+    def __init__(self): 
+        self.game = ScrabbleGame(players_count=0)
+
+    def get_player_count(self):
+        while True:
+            try:
+                player_count = self.get_valid_player_count_input()
+                if player_count is not None:
+                    return player_count
+            except ValueError:
+                print('Ingrese un número válido por favor.')
+            return None
+
+    def get_valid_player_count_input(self):
+        try:
+            player_count = int(input('Cantidad de jugadores (1-3): '))
+            if 1 <= player_count <= 3:
+                return player_count
+            else:
+                print('Número de jugadores no válido. Intente de nuevo.')
+                return None
+        except ValueError:
+            return None
--- test/test_scrabbleCLI.py
+++ test/test_scrabbleCLI.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from game.scrabbleCli import ScrabbleCli
 
 class TestScrabbleCli(unittest.TestCase):
+#Arreglar tests
     @patch('builtins.input', return_value='3')
     def test_get_player_count(self, input_patched):
         scrabble_cli = ScrabbleCli()
@@ -11,14 +12,16 @@ class TestScrabbleCli(unittest.TestCase):
     @patch('builtins.input', side_effect=['A', '3'])
     def test_player_count_wrong_input(self, input_patched):
         scrabble_cli = ScrabbleCli()
-        # 
-        #
-        self.assertEqual(scrabble_cli.get_player_count(), 3)
+        result = scrabble_cli.get_player_count()
+        self.assertIsNone(result)  # Verifica que el resultado sea None en caso de entrada incorrecta
 
     @patch('builtins.input', side_effect=['10', '1'])
     def test_player_count_max(self, input_patched):
         scrabble_cli = ScrabbleCli()
-        self.assertEqual(scrabble_cli.get_player_count(), 1)
+        #with self.assertRaises(ValueError):
+        #    scrabble_cli.get_player_count()
+
+        self.assertEqual(scrabble_cli.get_player_count(), None)
 
 if __name__ == "__main__":
     unittest.main()
